11d4f0e3448b1633b7f09fee58513a40f7f84bce
otmane <otmane.hanine@enim.ac.ma>
2023-06-01 00:10:25 +0100

    fixed some minor image overlay and logo pic problems

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -48,15 +48,18 @@
                         <!-- Single Banner  -->
                         <div class="col-lg-4 col-md-6 col-12">
                             <div class="single-banner">
+                                <div class="category_overlay">
                                 @if($cat->photo)
                                     <img src="{{$cat->photo}}" alt="{{$cat->photo}}">
                                 @else
                                     <img src="https://via.placeholder.com/600x370" alt="#">
                                 @endif
+                            </div>
                                 <div class="content">
                                     <h3>{{$cat->title}}</h3>
                                         <a href="{{route('product-cat',$cat->slug)}}">Discover Now</a>
                                 </div>
+
                             </div>
                         </div>
                     @endif
