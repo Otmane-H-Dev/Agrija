70bd12dce813825ed08b8717630224e6749b1f00
otmane <otmane.hanine@enim.ac.ma>
2023-06-13 06:48:21 +0100

    robots, and sitemap

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,18 @@ const mix = require('laravel-mix');
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
- |
+ |0
  */
+ mix.styles([
+    '/public/frontend/css/style.css',
+], '/public/frontend/css/style.min.css');
+
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();
+
+    mix.styles([
+        'resources/css/app.css',
+    ], 'public/css/app.min.css');
+    
